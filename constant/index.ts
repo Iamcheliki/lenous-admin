ce8b85cbf858e7ab947ca3811e8e48b10ff0f737
Hamid Roohi <hamidroohi71@gmail.com>
2025-01-15 17:39:05 +0330

    fix(links): fix server links issues

--- constant/index.ts
+++ constant/index.ts
@@ -1,2 +1,4 @@
-export const BASE_BACK_URL = process.env.NEXT_PUBLCI_SERVER_URL;
-export const BASE_SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
+export const BASE_BACK_URL = "https://lenous-server.liara.run/api/v1";
+export const BASE_SOCKET_URL = "https://lenous-server.liara.run";
+// export const BASE_BACK_URL = "http://localhost:3000/api/v1";
+// export const BASE_SOCKET_URL = "http://localhost:3000";
