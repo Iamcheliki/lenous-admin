b8d254a61baef88297c9c5ef5a29468caa5bbe18
Hamid Roohi <hamidroohi71@gmail.com>
2025-01-15 16:17:01 +0330

    update url

--- constant/index.ts
+++ constant/index.ts
@@ -1,1 +1,2 @@
-export const BASE_BACK_URL = "http://localhost:3000/api/v1";
+export const BASE_BACK_URL = process.env.NEXT_PUBLCI_SERVER_URL;
+export const BASE_SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
