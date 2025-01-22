05ea09ac3c6548b9b9e2791ac98011cf03f16f77
Hamid Roohi <hamidroohi71@gmail.com>
2025-01-22 18:42:36 +0330

    update links

--- constant/index.ts
+++ constant/index.ts
@@ -1,4 +1,4 @@
-// export const BASE_BACK_URL = "https://lenous-server.liara.run/api/v1";
-// export const BASE_SOCKET_URL = "https://lenous-server.liara.run";
-export const BASE_BACK_URL = "http://localhost:3000/api/v1";
-export const BASE_SOCKET_URL = "http://localhost:3000";
+export const BASE_BACK_URL = "https://lenous-server.liara.run/api/v1";
+export const BASE_SOCKET_URL = "https://lenous-server.liara.run";
+// export const BASE_BACK_URL = "http://localhost:3000/api/v1";
+// export const BASE_SOCKET_URL = "http://localhost:3000";
